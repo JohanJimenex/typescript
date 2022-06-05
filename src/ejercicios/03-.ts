af644ec12eb1da2778d8cb02dc6577a762e8605c
johanjimenex <johan.jimenez.23@gmail.com>
2022-06-05 19:24:11 +0200

    from dell laptop

---
+++ src/ejercicios/03-.ts
@@ -0,0 +1,54 @@
+//especificamo los tipos en los paramatrros
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+//especificamo los tipos en los paramatrros
+const sumar2 = function (a: number, b: number): number {
+    return a + b;
+};
+
+//especificamo los tipos en los paramatrros
+const sumar3 = (a: number, b: number): number => {
+    return a + b;
+};
+
+//otras formas de crear function
+const sumar4 = (a: number, b: number): number => a + b;
+
+const resultado = sumar(1220, 20);
+
+console.log(resultado.toString());
+
+//parametros con valor por defectos y opcionales con "?"
+function multiplicar(num1: number, num2?: number, base: number = 10) {
+
+    return num1 * base;
+}
+
+multiplicar(5, 2) // esto es igual a 50 ya que la funcion toma la base
+
+interface Player2 {
+
+    nombre: string,
+    puntosVida: number,
+    saltar: () => void
+
+}
+
+const jugador: Player2 = {
+    nombre: "johan",
+    puntosVida: 4,
+    saltar: function (): void {
+        console.log("saltando");
+
+    }
+}
+
+function curar(personaje: Player2, valorACurar: number): void {
+
+    personaje.puntosVida += 10
+
+}
+
+curar(jugador, 5);
